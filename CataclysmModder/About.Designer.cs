cbdac7edca4dfc182f66fc9d967a723696f09737
BrianMacIntosh <bmacbrian@pacbell.net>
2013-10-14 23:34:30 -0700

    Fixed a bug where recipe skills weren't saved.

--- CataclysmModder/About.Designer.cs
+++ CataclysmModder/About.Designer.cs
@@ -66,9 +66,9 @@
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.label2.Location = new System.Drawing.Point(13, 191);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(238, 13);
+            this.label2.Size = new System.Drawing.Size(233, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = " ver 0.1 rev 26.  For Cataclysm ver 0.7 (Lindqvist)";
+            this.label2.Text = " ver 0.1 rev 29.  For Cataclysm ver 0.8 (Romero)";
             // 
             // label3
             // 
